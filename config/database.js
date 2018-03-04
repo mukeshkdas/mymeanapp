d0625e46aa8350cb8fe563e0c1b0864054a2bf9d
Mukesh Kumar Das <mukeshk_das@infosys.com>
2018-03-04 14:23:57 +0530

    few changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/mymeanapp',
+    //database: 'mongodb://localhost:27017/mymeanapp',
+    database: 'mongodb://mongodb:27017/mymeanapp',
     //database: 'mongodb://mukeshdas:mukeshdas15@ds235418.mlab.com:35418/mymeanapp',    
     secret: 'yoursecret'
 }
